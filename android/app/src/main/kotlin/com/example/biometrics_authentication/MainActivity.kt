9a77f0e82ced006079494a1840c92f3d09e75020
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2025-03-31 14:37:34 +0200

    Setup Android & iOS Integration

--- android/app/src/main/kotlin/com/example/biometrics_authentication/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/biometrics_authentication/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.biometrics_authentication
-
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
