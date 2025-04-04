a61119a82bd1698b6dee17d74920ae58316ea3a4
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2025-04-04 19:20:38 +0200

    Update iOS and Android configurations for biometrics authentication

--- android/app/src/main/kotlin/com/example/biometrics_authentication/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/biometrics_authentication/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.biometrics_authentication
 import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {}
